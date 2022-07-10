cc7a37953114fd2aab2ef18f2b399f421afe5a2d
Mason John Hawver <mason.hawver@gmail.com>
2022-07-10 18:28:29 -0500

    Source Code Pro Font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font = "SourceCodePro-Bold.otf:size=6";
 static int borderpx = 20;
 
 /*
